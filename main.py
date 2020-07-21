ddc05e8de8f31a282d2441abaa7a5b65700f4615
Marcus Kueh <marcus.k@scclc.net>
2020-07-21 17:21:38 +0000

    Added for loop conditional problem

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-# First = input("Hello what is your First name? ")
-# answer = input("Hello"   + First )
-# Last = input("Hello" + First + "what is your Last name?" )
-# answer = input("Hello"  + First  + Last )
+# First = input("Hello What is your First Name? ")
+# Last = input("Hello what is your Last Name? ")
+# Answer= input("Hello" + First + Last)
 
 counter = 0
 for i in range(3) :
   counter += 1 
   print(counter)
+
+for x in range(20):
+  if x > 10:
+    print("greater than", x)
+  elif x == 10:
+    print("x is 10", x)
+  else:
+    print("less than 10", x)
+  
