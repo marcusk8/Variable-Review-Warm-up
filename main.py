3f5b89e60d40c18b7979006fa95e2db18c830b20
Marcus Kueh <marcus.k@scclc.net>
2020-07-21 17:01:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# First = input("Hello what is your First name? ")
+# answer = input("Hello"   + First )
+# Last = input("Hello" + First + "what is your Last name?" )
+# answer = input("Hello"  + First  + Last )
+
+counter = 0
+for i in range(3) :
+  counter += 1 
+  print(counter)
